55b1f25077b543a7d12ffdffef986d8d95830aa1
Gabriel <emilio.nunez.arenas@gmail.com>
2023-08-08 11:30:03 -0500

    chore more details

--- src/components/socialModal.jsx
+++ src/components/socialModal.jsx
@@ -4,16 +4,40 @@ const SocialModal = () => {
   return (
     <div className=" h-14 rounded-xl w-80 bg-gray-600 bg-opacity-40 backdrop-blur-md fixed bottom-10 flex z-30 items-center justify-around md:w-96 md:h-16">
       <div className="rounded-xl w-11 h-11 bg-gray-400/20 grid place-content-center hover:scale-125 ease-in duration-300">
-        <BsGithub fontSize={30} color="gray" />
+        <a
+          href="https://github.com/NunezGabriel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub fontSize={30} color="gray"></BsGithub>
+        </a>
       </div>
       <div className="rounded-xl w-11 h-11 bg-gray-400/20 grid place-content-center hover:scale-125 ease-in duration-300">
-        <BsLinkedin fontSize={30} color="gray" />
+        <a
+          href="https://www.linkedin.com/in/gabriel-nu%C3%B1ez-arenas/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin fontSize={30} color="gray"></BsLinkedin>
+        </a>
       </div>{" "}
       <div className="rounded-xl w-11 h-11 bg-gray-400/20 grid place-content-center hover:scale-125 ease-in duration-300">
-        <BsTwitter fontSize={30} color="gray" />
+        <a
+          href="https://twitter.com/gabrielnunez_23"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsTwitter fontSize={30} color="gray"></BsTwitter>
+        </a>
       </div>{" "}
       <div className="rounded-xl w-11 h-11 bg-gray-400/20 grid place-content-center hover:scale-125 ease-in duration-300">
-        <BsInstagram fontSize={30} color="gray" />
+        <a
+          href="https://www.instagram.com/gabri3l_nun3z/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsInstagram fontSize={30} color="gray"></BsInstagram>
+        </a>
       </div>
     </div>
   );
--- src/pages/allProyects.jsx
+++ src/pages/allProyects.jsx
@@ -1,14 +1,59 @@
-import MainContainer from "../components/mainContainer";
-import NavBar from "../components/navBar";
+import keepable from "../assets/keepable.png";
+import gTHome from "../assets/gTHome.png";
+import pokedex from "../assets/pokedex.png";
+import eatable from "../assets/eatable.png";
+import { Link } from "react-router-dom";
+import vectorG from "../assets/vectorG.png";
+import grridds from "../assets/grridds.gif";
 
 const AllProyects = () => {
-  <div>
-    <NavBar />
-    <MainContainer>
-      <h1 className="text-gray-200">All my proyects</h1>
-    </MainContainer>
-    ;
-  </div>;
+  return (
+    <div>
+      <div className="flex fixed top-0 left-0 w-full h-20 backdrop-blur-xl border-b-[0.1px] z-30 border-gray-400/40 mb-4 py-2 ">
+        <div className="max-w-4xl w-full mx-auto flex items-center justify-center px-2">
+          <Link to={"/"} style={{ textDecoration: "none" }}>
+            <img src={vectorG} alt="cube image" className="w-24 h-24" />
+          </Link>
+        </div>
+      </div>
+
+      <div className="max-w-6xl mx-auto mt-24 p-4 grid justify-items-start relative rounded-xl">
+        <div className=" p-4 rounded-2xl">
+          <h1 className="text-gray-400 text-3xl">All my proyects</h1>
+        </div>
+        <div className="grid mt-4 gap-6 md:flex md:flex-wrap justify-center">
+          <div className="w-full h-64 bg-black bg-opacity-40 rounded-2xl md:w-[240px] md:h-56 cursor-pointer overflow-hidden">
+            <img
+              src={gTHome}
+              alt=""
+              className="h-full w-full object-cover object-center "
+            />
+          </div>
+          <div className="w-full h-64 bg-black bg-opacity-40 rounded-2xl md:w-[240px] md:h-56 cursor-pointer overflow-hidden">
+            <img
+              src={pokedex}
+              alt=""
+              className="h-full w-full object-cover object-center "
+            />
+          </div>
+          <div className="w-full h-64 bg-black bg-opacity-40 rounded-2xl md:w-[240px] md:h-56 cursor-pointer overflow-hidden">
+            <img
+              src={eatable}
+              alt=""
+              className="h-full w-full object-cover object-center "
+            />
+          </div>
+          <div className="w-full h-64 bg-black bg-opacity-40 rounded-2xl md:w-[240px] md:h-56 cursor-pointer overflow-hidden ">
+            <img
+              src={keepable}
+              alt=""
+              className="h-full w-full object-cover object-left "
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllProyects;
