ada81222e617c0beea7ff808610968720588d067
Gabriel <emilio.nunez.arenas@gmail.com>
2023-08-10 23:33:07 -0500

    chore: proyects routering + remove trash files

--- src/components/socialModal.jsx
+++ src/components/socialModal.jsx
@@ -2,7 +2,7 @@ import { BsGithub, BsLinkedin, BsTwitter, BsInstagram } from "react-icons/bs";
 
 const SocialModal = () => {
   return (
-    <div className=" h-14 rounded-xl w-80 bg-gray-600 bg-opacity-40 backdrop-blur-md fixed bottom-10 flex z-30 items-center justify-around md:w-96 md:h-16">
+    <div className=" h-14 rounded-xl w-80 bg-gray-600 bg-opacity-40 backdrop-blur-md fixed bottom-10 flex z-30 items-center justify-around md:w-16 md:h-80 md:right-[100px] md:top-44 md:flex-col ">
       <div className="rounded-xl w-11 h-11 bg-gray-400/20 grid place-content-center hover:scale-125 ease-in duration-300">
         <a
           href="https://github.com/NunezGabriel"
